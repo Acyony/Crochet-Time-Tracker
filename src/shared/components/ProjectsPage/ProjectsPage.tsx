b5579977338202528abf0a5c78f464c32a1dc3ce
alcione <alcionefribeiro@gmail.com>
2025-01-07 21:39:49 +0100

    add delete button

--- src/shared/components/ProjectsPage/ProjectsPage.tsx
+++ src/shared/components/ProjectsPage/ProjectsPage.tsx
@@ -54,18 +54,22 @@ const ProjectList = () => {
                             <p>Look how many great projects you did ==^.^==!</p>
                         </div>
                     </div>
+
                     <ul className="list-group mt-5 ">
                         {projects.map((project) => (
                             <li
                                 key={project.id}
-                                className="list-group-item mt-2"
+                                className="list-group-item mt-2 d-flex justify-content-between align-items-center"
                                 style={{cursor: "pointer"}}
                                 onClick={() => handleProjectClick(project)}
                             >
                                 {project.name}
+                                <button type="button" className="btn btn-danger">Danger</button>
                             </li>
                         ))}
                     </ul>
+
+
                 </div>
             </main>
             <FooterComponent/>
