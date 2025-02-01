bb798d8921d3ebc9441e942352854eb6a31b8c4e
alcione <alcionefribeiro@gmail.com>
2025-02-01 17:42:13 +0100

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,41 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
-  description: "Using Bootstrap in Next.js",
+    title: "Timer Track App",
+    description: "A simple and intuitive Time Tracker!",
+    icons: {
+        icon: "/favicon.png",
+    },
 };
 export default function RootLayout({
-children,
-}: Readonly<{
+  children,
+   }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en" suppressHydrationWarning={true}>
         <Head>
             {/* Metadata */}
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>My Next.js App</title>
-            <link rel="icon" href="/favicon.ico" />
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>My Timer Track App</title>
+            <link rel="icon" href="/favicon.png"  type="image/png"/>
         </Head>
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
-            {children}
+        {children}
         </body>
         </html>
     );
