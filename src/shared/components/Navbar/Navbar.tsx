6b397435a6986b03bb3c4d3a34c02afe649222bc
alcione <alcionefribeiro@gmail.com>
2025-02-02 09:33:11 +0100

    Add logo to nav

--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Link from "next/link";
+import Image from "next/image";
+
 
 export const NavbarComponent = () => {
 
@@ -9,7 +11,10 @@ export const NavbarComponent = () => {
         <nav className="navbar navbar-light bg-light w-100">
             <div className="container-fluid d-flex align-items-center justify-content-start">
                 <div>
-                    <a className="navbar-brand me-4" href="#">TimeTracker</a>
+                    <a className="navbar-brand d-flex align-items-center" href="/home">
+                        <Image src="/favicon.png" alt="Logo" width="30" height="30"/>
+                        <span>TimeTracker</span>
+                    </a>
                 </div>
                 <div>
                     <ul className="navbar-nav d-flex flex-row">
